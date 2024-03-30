658d7fa4af46b3cb0cece8e087e30bf5ad793bf2
Sguoli <sebastianhwgu@gmail.com>
2024-03-30 11:17:23 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -11,37 +11,24 @@ import Col from 'react-bootstrap/Col';
 function App() {
   return (
     <div>
-
-    <div className='container1-lg' id='head-of-page'>
-      <div className='row1'>
-        <header>      
-          <h1 className='text-center' id='header'>OSU Highlights</h1>
-        </header>
-      </div>
-
-      <div className='row2'>
-        <h2 className='text-center' id='sport-name'> Sports Name </h2>
-      </div>
-
-      <div className='row3'>
-        <div className='sort-container'>
-          <Dropdown align = "end">
-            <Dropdown.Toggle align = 'end' variant="success" id="sort-dropdown">
-              Sort By
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item href="#/action-1">Most Viewed</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Least Viewed</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Newest</Dropdown.Item>
-              <Dropdown.Item href="#/action-4">Oldest</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-      </div>
-    </div>
-
-
+    <Container>
+      <Row> OSU </Row>
+      <Row> SPORTS NAME</Row>
+      <Row> 
+        <Dropdown align = "end">
+          <Dropdown.Toggle align = 'end' variant="success" id="sort-dropdown">
+            Sort By
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Most Viewed</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Least Viewed</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Newest</Dropdown.Item>
+            <Dropdown.Item href="#/action-4">Oldest</Dropdown.Item>
+          </Dropdown.Menu>
+         </Dropdown>
+      </Row>
+    </Container>
+    
     <Container>
       <Row>
         <Col>Filter By</Col>
@@ -49,7 +36,7 @@ function App() {
       </Row>
     </Container>
 
-  </div>
+    </div>
   );
 }
 
