171f1b810fed70788b912770d614368419c14098
Sguoli <sebastianhwgu@gmail.com>
2024-03-30 09:09:20 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
+import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import $ from 'jquery';
+import Popper from 'popper.js';
 
 
 function App() {
