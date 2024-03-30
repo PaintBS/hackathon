60969a9e105344cd500978f9c0e8051254f5e9af
Sguoli <sebastianhwgu@gmail.com>
2024-03-30 16:36:54 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -7,36 +7,48 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import Image from 'react-bootstrap/Image';
 
 
 function App() {
   return (
     <div>
-    <Container>
-      <Row id='row1'> OSU </Row>
-      <Row id='row2'> SPORTS NAME</Row>
-      <Row id='row3'> 
-        <DropdownButton
+
+    <div id='header'>
+      <Container id='header_container'>
+        <div id='header2'>
+      <Row id='row1' className='justify-content-center' width="200px"> 
+        <Image rounded src='https://brand.okstate.edu/site-files/images/brand-guide/primary-brand.png' 
+        id='osu_logo' >
+        </Image>
+      </Row>
+      <Row id='row2' className='justify-content-center'> SPORTS NAME</Row>
+      <Row id='row3' align = "end"> 
+        <DropdownButton 
           id="dropdown-button-sort"
           variant="secondary"
           title="Sort"
           className="mt-2"
           data-bs-theme="light"
         >
-          <Dropdown.Item href="#/action-1" active> Highest View </Dropdown.Item>
+          <Dropdown.Item href="#/action-1"> Highest View </Dropdown.Item>
           <Dropdown.Item href="#/action-2">Lowest View</Dropdown.Item>
           <Dropdown.Item href="#/action-3">Oldest</Dropdown.Item>
           <Dropdown.Item href="#/action-4">Recent</Dropdown.Item>
         </DropdownButton>
       </Row>
+      </div>
     </Container>
-    
-    <Container>
-      <Row>
-        <Col>Filter By</Col>
-        <Col>VIDEOS HERE</Col>
+    </div>
+
+    <div>
+    <Container bg>
+      <Row id='row4'>
+        <Col id="col1">Filter By</Col>
+        <Col id="col2" align="center">VIDEOS HERE</Col>
       </Row>
     </Container>
+    </div>
 
     </div>
   );
