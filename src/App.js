f5a38cc39a146cef81c59fc4490bd0b3c09f0798
Sguoli <sebastianhwgu@gmail.com>
2024-03-30 09:30:49 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Dropdown from 'react-bootstrap/Dropdown';
 function App() {
   return (
     <div>
+
+      <div>
+      <body>
       <h1>Hello There</h1>  
       <Dropdown>
       <Dropdown.Toggle variant="success" id="sort-dropdown">
@@ -21,6 +24,26 @@ function App() {
         <Dropdown.Item href="#/action-4">Oldest</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
+    </body>
+    </div>
+    <body>
+    <Dropdown>
+      <Dropdown.Toggle variant="success" id="filter-dropdown">
+        Sort By
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Most View</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Least View</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Recent</Dropdown.Item>
+        <Dropdown.Item href="#/action-4">Oldest</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+    </body>
+    <div>
+
+    </div>
+
     </div>
     
   );
