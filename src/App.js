7eeb4e4cbb5fdc14e68edd4bdf250992f8b8ad1b
Sguoli <sebastianhwgu@gmail.com>
2024-03-30 08:34:34 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import Dropdown from 'react-bootstrap/Dropdown';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="OSU Sports">
-      <header className="MainPage">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-      </header>
-    </div>
+    <h1>Hello There</h1>  
   );
 }
 
