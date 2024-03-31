9e16748fed3197633c362f7c11b59b750f88f6b4
Sguoli <sebastianhwgu@gmail.com>
2024-03-31 13:46:24 -0500

    commit

--- src/MainPage.js
+++ src/MainPage.js
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './OsuLogoWhite.png'; // import the image
 import './MainPage.css'; // import the css file
 import { Link } from 'react-router-dom'; // import the Link component from react-router-dom
+import { Container, Row } from 'react-bootstrap';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Col from 'react-bootstrap/Col';
 
 
 
@@ -14,17 +18,31 @@ export default function MainPage(){
                 </div>
                 <div>
                 <ul id='home-links'>
-                    <li id='Home'><Link to="/">Home</Link></li>
-                    <li id='Sports' text-d><Link to="/Football">Football</Link></li>
+                    <li className='nav-list'  id='nav_link'><Link to="/">
+                        Home</Link>
+                    </li>
+                    <li className='nav-list'  id='nav_link' text-d><Link to="/Football">
+                        Football</Link>
+                    </li>
+        
+        
+                    
+                    <Container>
+                        <Row>
+                            <Col>
+                            <DropdownButton title="Football">
+                                <Dropdown.Item id='sport_select_menu'> 
+                                <Link to="/Football">Football 
+                                </Link></Dropdown.Item>
+                                <Dropdown.Item id='sport_select_menu'>Basketball</Dropdown.Item>
+                            </DropdownButton>
+                            </Col>
+                        </Row>
+                    </Container>
+                   
                 </ul>
                 {/*
-                <DropdownButton>
-
-                    <Dropdown.Item id='sport_select_menu'>Football</Dropdown.Item>
-                    <Dropdown.Item id='sport_select_menu'>FILL</Dropdown.Item>
-                    <Dropdown.Item id='sport_select_menu'>FILL</Dropdown.Item>
-                    
-                    </DropdownButton>
+                
     */}
 
                 </div>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Football from './Football';
 
 
 import reportWebVitals from './reportWebVitals';
