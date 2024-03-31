120e3236736033748e513d763f0e6ae8fdad6bf0
Keegneal <92490783+Keegneal@users.noreply.github.com>
2024-03-30 23:50:13 -0700

    added some style changes

--- src/MainPage.js
+++ src/MainPage.js
@@ -9,7 +9,7 @@ export default function MainPage(){
                 <img src={logo} alt="OSU Logo" /> {/* use the imported image */}
             </div>
             <div>
-            <ul id='home_links'>
+            <ul id='home-links'>
                 <li id='Home'><a href="/">Home</a></li>
                 <li id='About'><a href="/players">About</a></li>
                 <li id='Sports' text-d><a href="/Sports">Sports</a></li>
