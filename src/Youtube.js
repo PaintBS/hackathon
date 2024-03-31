df2a63cf566b284d2393293ad106025f54ef5381
Keegneal <92490783+Keegneal@users.noreply.github.com>
2024-03-31 09:41:25 -0500

    fix api

--- src/Youtube.js
+++ src/Youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyDLFRm8YtoDxtAOXdNmfkIeGAiA1i8f66A'; // Replace 'YOUR_API_KEY' with your actual YouTube API key
+const KEY = 'AIzaSyC5ZBqkwZOrd3QUtAIS9aM5DWIciSx_eEI'; // Replace 'YOUR_API_KEY' with your actual YouTube API key
 
 // Create and export the Axios instance directly
 const youtubeApi = axios.create({
